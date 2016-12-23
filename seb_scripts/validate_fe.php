682ab84b5535a304f733259e28622dfb5bad3ab2
vb-expert <vb-expert@yandex.ru>
2016-12-23 02:38:49 +0200

    1

--- seb_scripts/validate_fe.php
+++ seb_scripts/validate_fe.php
@@ -3,7 +3,7 @@ header('Content-Type: text/html; charset=utf-8');
 ?>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 </head>
 <body>
 
