7e5401cf7b856ce6f4532e5b1aa3587fe564278d
1 <1@ADMIN>
2017-03-15 18:40:16 +0200

    create dir tree 5.1

--- winforms5.1/winforms5.1.0/Properties/Resources.Designer.cs
+++ winforms5.1/winforms5.1.0/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WindowsApplication0.Properties {
+namespace WindowsApplication1.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace WindowsApplication0.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WindowsApplication0.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WindowsApplication1.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- winforms5.1/winforms5.1.0/Properties/Settings.Designer.cs
+++ winforms5.1/winforms5.1.0/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WindowsApplication0.Properties {
+namespace WindowsApplication1.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
