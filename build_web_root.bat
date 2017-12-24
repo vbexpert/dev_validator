fc54c2631f15f593e60d2c129a3524ded176a9ec
Admin <Admin@HDET>
2017-12-24 03:37:30 +0200

    + mass updates + added validation procedure for BackGroundImage in form via Object == Null + unit tests + all forms tasks

--- build_web_root.bat
+++ build_web_root.bat
@@ -14,30 +14,8 @@ rem copy all devroot:
 xcopy /S /A dev_root\*.* web_root\*.*
 
 rem ------------------------------------------------------------------------------->
-set s_task_name="01_Form_01"
-
-rem 2. путь до базовой директории проекта таска:
-set s_task_dev_dir="tasks_source_code\%s_task_name%"
-set s_task_web_root_dir=web_root\tasks\%s_task_name%
-rem 3. сборка для web_root:
-mkdir %s_task_web_root_dir%
-xcopy /S /A %s_task_dev_dir% %s_task_web_root_dir%
-rem ------------------------------------------------------------------------------->
-set s_task_name="01_Form_02"
-
-rem 2. путь до базовой директории проекта таска:
-set s_task_dev_dir="tasks_source_code\%s_task_name%"
-set s_task_web_root_dir=web_root\tasks\%s_task_name%
-rem 3. сборка для web_root:
-mkdir %s_task_web_root_dir%
-xcopy /S /A %s_task_dev_dir% %s_task_web_root_dir%
-rem ------------------------------------------------------------------------------->
-set s_task_name="01_Form_03"
-
-rem 2. путь до базовой директории проекта таска:
-set s_task_dev_dir="tasks_source_code\%s_task_name%"
-set s_task_web_root_dir=web_root\tasks\%s_task_name%
-rem 3. сборка для web_root:
-mkdir %s_task_web_root_dir%
-xcopy /S /A %s_task_dev_dir% %s_task_web_root_dir%
+call deploy_task.bat 01_Form_01
+call deploy_task.bat 01_Form_02
+call deploy_task.bat 01_Form_03
+call deploy_task.bat 01_Form_04
 rem ------------------------------------------------------------------------------->
