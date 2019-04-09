4fab66d7a77e961d3c03e689d6d51458427c36ae
ztshat <ztshat@gmail.com>
2019-04-09 17:03:02 +0300

    Add tasks Add task set: 08_Physics Add: 08_Energy Fix: 07 App_Control

--- web_root/tasks/07_App_Control/task.php
+++ web_root/tasks/07_App_Control/task.php
@@ -37,55 +37,113 @@ $cls_Task->_block_end();
 //---------------------------------------->
 $cls_Task->_block_start("Додати об'єкт: PictureBox", "_add_object.png");
 $cls_Task->_add_property("Exists", "Наявність", "");
-$cls_Task->_add_property("Name", "Ім'я", "pictureBox1");
-//$cls_Task->_add_property("Width", "Розмір", "335");
-//$cls_Task->_add_property("Height", "Розмір", "85");
-//$cls_Task->_add_property("BackgroundImage", "Фонове зображення", "1.png");
-//$cls_Task->_add_property("BackgroundImageLayout", "Спосіб відображення фонового малюнку", "Zoom");
+$cls_Task->_add_property("Name", "Ім'я", "pb_Image");
+$cls_Task->_add_property("Width", "Розмір", "335");
+$cls_Task->_add_property("Height", "Розмір", "85");
+$cls_Task->_add_property("BackgroundImage", "Фонове зображення", "1.png");
+$cls_Task->_add_property("BackgroundImageLayout", "Спосіб відображення фонового малюнку", "Zoom");
 $cls_Task->_add_screen("Формула", "1.png");
 $cls_Task->_block_end();
 //---------------------------------------->
+$cls_Task->_block_start("Додати об'єкт: Label", "_add_object.png");
+$cls_Task->_add_property("Exists", "Наявність", "");
+$cls_Task->_add_property("Name", "Ім'я", "label2");
+$cls_Task->_add_property("Text", "Текст", "Введіть значення аргументу:");
+$cls_Task->_add_property("Font.Size", "Розмір шрифту", "14");
+$cls_Task->_block_end();
+//---------------------------------------->
+$cls_Task->_block_start("Додати об'єкт: Label", "_add_object.png");
+$cls_Task->_add_property("Exists", "Наявність", "");
+$cls_Task->_add_property("Name", "Ім'я", "label4");
+$cls_Task->_add_property("Text", "Текст", "Значення функції:");
+$cls_Task->_add_property("Font.Size", "Розмір шрифту", "14");
+$cls_Task->_block_end();
+//---------------------------------------->
+$cls_Task->_block_start("Додати об'єкт: Label", "_add_object.png");
+$cls_Task->_add_property("Exists", "Наявність", "");
+$cls_Task->_add_property("Name", "Ім'я", "label3");
+$cls_Task->_add_property("Text", "Текст", "x=");
+$cls_Task->_add_property("Font.Size", "Розмір шрифту", "14");
+$cls_Task->_block_end();
+//---------------------------------------->
+$cls_Task->_block_start("Додати об'єкт: Label", "_add_object.png");
+$cls_Task->_add_property("Exists", "Наявність", "");
+$cls_Task->_add_property("Name", "Ім'я", "label5");
+$cls_Task->_add_property("Text", "Текст", "y=");
+$cls_Task->_add_property("Font.Size", "Розмір шрифту", "14");
+$cls_Task->_block_end();
+//---------------------------------------->
+$cls_Task->_block_start("Додати новий компонент: TextBox", "_add_object.png");
+$cls_Task->_add_property("Exists", "Наявність", "");
+$cls_Task->_add_property("Name", "Ім'я поля для вводу", "tb_Input");
+$cls_Task->_add_property("Text", "Текст ", "");
+$cls_Task->_block_end();
+//---------------------------------------->
+$cls_Task->_block_start("Додати новий компонент: TextBox", "_add_object.png");
+$cls_Task->_add_property("Exists", "Наявність", "");
+$cls_Task->_add_property("Name", "Ім'я поля для вводу", "tb_Result");
+$cls_Task->_add_property("Text", "Текст ", "");
+$cls_Task->_block_end();
+//---------------------------------------->
 $cls_Task->_block_start("Додати об'єкт: Button", "_add_object.png");
-$cls_Task->_add_property("Name", "Ім'я", "btn_ChangeText");
+$cls_Task->_add_property("Name", "Ім'я", "btn_Calculate");
 $cls_Task->_block_end();
 //---------------------------------------->
-$cls_Task->_block_start("Сгенерувати подію <b>_Click</b> для кнопки: <b>btn_ChangeText</b>", "_add_event.png");
-$cls_Task->_add_code_validated("Код події:", "private void btn_ChangeText_Click(object sender, EventArgs e)
+$cls_Task->_block_start("Сгенерувати подію <b>_Click</b> для кнопки: <b>btn_Calculate</b>", "_add_event.png");
+$cls_Task->_add_code_validated("Код події:", "private void btn_Calculatet_Click(object sender, EventArgs e)
 {
     
 }", "");
 $cls_Task->_block_end();
 //---------------------------------------->
-$cls_Task->_block_start("Написати код події <b>btn_ChangeText_click</b>:", "_add_code.png");
-$cls_Task->_add_code_validated("Вписати код:", "private void btn_ChangeText_Click(object sender, EventArgs e)
-{
-    btn_ChangeText.Text = \"Напис на кнопці змінено!!!\";
-}", "");
-//---------------------------------------->
-$cls_Task->_block_start("Додати об'єкт: CheckBox", "_add_object.png");
-$cls_Task->_add_property("Name", "Ім'я", "checkBox1");
+$cls_Task->_block_start("Написати код події <b>btn_Calculate_Click</b>:", "_add_code.png");
+$cls_Task->_add_code_validated("Вписати код:", "
+double x, y;
+x = double.Parse(tb_Input.Text);
+y = (Math.Sqrt(x + 2) - Math.Pow(x, 5)) / (Math.Pow(x, 2) - 7 * x + 4) + 2019;
+tb_Result.Text = y.ToString();
+", "");
+$cls_Task->_add_property("tb_Result.Text", "Текст ", "<i>Розраховані програмою значення</i>");
 $cls_Task->_block_end();
 //---------------------------------------->
-$cls_Task->_block_start("Сгенерувати подію <b>CheckedChanged</b> для cb: <b>1111</b>", "_add_event.png");
-$cls_Task->_add_code_validated("Код події:", "private void checkBox1_CheckedChanged(object sender, EventArgs e)
+$cls_Task->_block_start("Додати об'єкт: Button", "_add_object.png");
+$cls_Task->_add_property("Name", "Ім'я", "btn_Clear");
+$cls_Task->_block_end();
+//---------------------------------------->
+$cls_Task->_block_start("Сгенерувати подію <b>_Click</b> для кнопки: <b>btn_Clear</b>", "_add_event.png");
+$cls_Task->_add_code_validated("Код події:", "private void btn_Clear_Click(object sender, EventArgs e)
 {
     
 }", "");
+$cls_Task->_block_end();
+//---------------------------------------->
+$cls_Task->_block_start("Написати код події <b>btn_Clear_Click</b>:", "_add_code.png");
+$cls_Task->_add_code_validated("Вписати код:", "
+tb_Input.Clear();
+tb_Result.Clear();
+", "");
+$cls_Task->_add_property("tb_Input.Text", "Текст", "");
+$cls_Task->_add_property("tb_Result.Text", "Текст", "");
+$cls_Task->_block_end();
+//---------------------------------------->
+$cls_Task->_block_start("Додати об'єкт: Button", "_add_object.png");
+$cls_Task->_add_property("Name", "Ім'я", "btn_Close");
+$cls_Task->_block_end();
+//---------------------------------------->
+$cls_Task->_block_start("Сгенерувати подію <b>_Click</b> для кнопки: <b>btn_Close</b>", "_add_event.png");
+$cls_Task->_add_code_validated("Код події:", "private void btn_Close_Click(object sender, EventArgs e)
+{
+    
+}", "");
+$cls_Task->_block_end();
+//---------------------------------------->
+$cls_Task->_block_start("Написати код події <b>btn_Close_Click</b>:", "_add_code.png");
+$cls_Task->_add_code_validated("Вписати код:", "
+Close();
+", "");
 $cls_Task->_block_end();
 //---------------------------------------->
 
-$cls_Task->_block_start("Додати об'єкт: RadioButton", "_add_object.png");
-$cls_Task->_add_property("Name", "Ім'я", "radioButton1");
-$cls_Task->_block_end();
-//---------------------------------------->
-$cls_Task->_block_start("Сгенерувати подію <b>CheckedChanged</b> для rb: <b>1111</b>", "_add_event.png");
-$cls_Task->_add_code_validated("Код події:", "private void radioButton1_CheckedChanged(object sender, EventArgs e)
-{
-    
-}", "");
-$cls_Task->_block_end();
-
-//---------------------------------------->
 $cls_Task->s_learn_url = "https://learn.ztu.edu.ua/mod/page/view.php?id=10041";
 $cls_Task->s_youtube_url = "https://youtu.be/hP0UsFULLuU";
 $cls_Task->s_discuss_url = "https://www.facebook.com/groups/1487277621317029/";
